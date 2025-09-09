7c187e4dfbe97edf01b17db73edf4b1b62d3c970
Finn <lehuynhtuong13@gmail.com>
2025-09-09 23:24:47 +0700

    Initial commit - Restaurant Management solution

---
+++ RestaurantManagement.Api/Program.cs
@@ -0,0 +1,45 @@
+using Microsoft.EntityFrameworkCore;
+using RestaurantManagement.Infrastructure.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// PostgreSQL connection
+builder.Services.AddDbContext<RestaurantDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Add Controllers
+builder.Services.AddControllers();
+
+// Swagger
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll",
+        policy => policy
+            .AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader());
+});
+
+
+var app = builder.Build();
+
+// Configure HTTP pipeline
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseCors("AllowAll");
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ RestaurantManagement.Infrastructure/Data/RestaurantDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace RestaurantManagement.Infrastructure.Data
+{
+    public class RestaurantDbContext : DbContext
+    {
+        public RestaurantDbContext(DbContextOptions<RestaurantDbContext> options) : base(options) { }
+
+    }
+}
