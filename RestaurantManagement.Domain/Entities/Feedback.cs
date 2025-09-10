d25a4777315283c47dbf61752e566f575f3b7c01
Finn <lehuynhtuong13@gmail.com>
2025-09-10 23:17:38 +0700

    add cart, cartdetail, payment, paymentdetail, feedback and update user, orderdetail
    add cart, cartdetail, payment, paymentdetail, feedback and update user, orderdetail

---
+++ RestaurantManagement.Domain/Entities/Feedback.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantManagement.Domain.Entities
+{
+    namespace RestaurantManagement.Domain.Entities
+    {
+        public class Feedback
+        {
+            public int Id { get; set; }
+            public int UserId { get; set; }
+            public User User { get; set; } = null!;
+            public int Rating { get; set; } 
+            public string? Comment { get; set; }
+
+            public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        }
+    }
+
+}
--- RestaurantManagement.Domain/Entities/User.cs
+++ RestaurantManagement.Domain/Entities/User.cs
@@ -1,4 +1,6 @@
-﻿namespace RestaurantManagement.Domain.Entities
+﻿using RestaurantManagement.Domain.Entities.RestaurantManagement.Domain.Entities;
+
+namespace RestaurantManagement.Domain.Entities
 {
     public enum UserRole
     {
@@ -7,19 +9,36 @@
         Customer
     }
 
+    public enum UserStatus
+    {
+        Active,
+        Inactive,
+        Suspended
+    }
+
     public class User
     {
         public int Id { get; set; }
         public string FullName { get; set; } = null!;
         public string Email { get; set; } = null!;
         public string PasswordHash { get; set; } = null!;
+
+        public string? Phone { get; set; }
+        public string? Address { get; set; }
+
         public UserRole Role { get; set; }
+        public UserStatus Status { get; set; } = UserStatus.Active;
+
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime? UpdatedAt { get; set; }
+        public bool IsDeleted { get; set; } = false;
 
         // Navigation
         public StaffProfile? StaffProfile { get; set; }
         public ICollection<Reservation> Reservations { get; set; } = new List<Reservation>();
         public ICollection<Order> Orders { get; set; } = new List<Order>();
+        public ICollection<Feedback> Feedbacks { get; set; } = new List<Feedback>();
+        public ICollection<Cart> Carts { get; set; } = new List<Cart>();
     }
 
 }
