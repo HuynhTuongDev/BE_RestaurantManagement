e8c7b604f19b67f187a9fc85f0df95d38c66633e
Nguyennu12 <mynu.thcscc@gmail.com>
2025-09-26 23:38:59 +0700

    define IOrderService interface

---
+++ RestaurantManagement.Application/Services/IOrderService.cs
@@ -0,0 +1,21 @@
+﻿using RestaurantManagement.Domain.DTOs;
+using RestaurantManagement.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantManagement.Application.Services
+{
+    public interface IOrderService
+    {
+        Task<OrderResponse> CreateOrderAsync(OrderCreateRequest request);
+        Task<OrderListResponse> GetAllOrdersAsync();
+        Task<OrderDto?> GetOrderByIdAsync(int id, int? userId = null);
+        Task<OrderListResponse> SearchOrdersAsync(string keyword);
+        Task<OrderResponse> UpdateOrderAsync(int id, OrderUpdateRequest request);
+        Task<bool> CancelOrderAsync(int id, int? userId, bool isCustomerRequest);
+        Task<OrderStatus?> GetOrderStatusAsync(int id, int? userId = null);
+    }
+}
