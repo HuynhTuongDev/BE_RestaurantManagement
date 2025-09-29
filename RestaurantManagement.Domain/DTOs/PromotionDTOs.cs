ecb14eafb6535b8f3a2297307de2ad59585c2db2
unknown <email_github_cua_m@example.com>
2025-09-29 13:50:59 +0700

    add promotion DTOs

---
+++ RestaurantManagement.Domain/DTOs/PromotionDTOs.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace RestaurantManagement.Domain.DTOs
+{
+    public class PromotionCreateDto
+    {
+        public string Code { get; set; } = null!;
+        public string? Description { get; set; }
+        public decimal Discount { get; set; } // ví d? 10 = 10%
+        public DateTimeOffset StartDate { get; set; }
+        public DateTimeOffset EndDate { get; set; }
+
+    }
+
+    public class PromotionDto
+    {
+        public int Id { get; set; }
+        public string Code { get; set; } = null!;
+        public string? Description { get; set; }
+        public decimal Discount { get; set; }
+        public DateTimeOffset StartDate { get; set; }
+        public DateTimeOffset EndDate { get; set; }
+        public string Status { get; set; } = string.Empty;
+    }
+}
