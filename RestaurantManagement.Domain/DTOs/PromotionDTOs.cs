3c92ff3e45022ff46c19fdf492d730fa627a410c
unknown <email_github_cua_m@example.com>
2025-09-30 10:09:42 +0700

    entities

--- RestaurantManagement.Domain/DTOs/PromotionDTOs.cs
+++ RestaurantManagement.Domain/DTOs/PromotionDTOs.cs
@@ -7,8 +7,8 @@ namespace RestaurantManagement.Domain.DTOs
         public string Code { get; set; } = null!;
         public string? Description { get; set; }
         public decimal Discount { get; set; } // ví d? 10 = 10%
-        public DateTimeOffset StartDate { get; set; }
-        public DateTimeOffset EndDate { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
 
     }
 
@@ -18,8 +18,8 @@ namespace RestaurantManagement.Domain.DTOs
         public string Code { get; set; } = null!;
         public string? Description { get; set; }
         public decimal Discount { get; set; }
-        public DateTimeOffset StartDate { get; set; }
-        public DateTimeOffset EndDate { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
         public string Status { get; set; } = string.Empty;
     }
 }
